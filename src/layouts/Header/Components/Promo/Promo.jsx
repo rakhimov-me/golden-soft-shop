be1e7b48bc55640faaf9a92abee22726961f1ef0
Rakhimov Timur <rakhimov.timur0510@gmail.com>
2025-07-31 08:52:41 +0500

    <svg /> components changed to SVGR imports. 'Promo' block devidet to certain .jsx object

---
+++ src/layouts/Header/Components/Promo/Promo.jsx
@@ -0,0 +1,18 @@
+import './Promo.scss'
+import React from "react";
+
+const Promo = (props) => {
+  const {} = props
+
+  return (
+    <section className="promo">
+      <div className="promo-body container">
+        <p></p>
+        <p className="=promo-text">Скидка 10% по промокоду “ZAMOK” на все заказы до 10.09</p>
+        <a href="#">Обратный звонок</a>
+      </div>
+    </section>
+  )
+}
+
+export default Promo
